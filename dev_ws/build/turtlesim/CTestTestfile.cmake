ffba594100285d639000464779d321f14ad61384
Hiroaki-K4 <49no19@gmail.com>
2021-01-26 22:00:32 +0900

    first commit

---
+++ dev_ws/build/turtlesim/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/hiroaki-k4/my_project/ros2/dev_ws/src/ros_tutorials/turtlesim
+# Build directory: /home/hiroaki-k4/my_project/ros2/dev_ws/build/turtlesim
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("turtlesim__py")
---
+++ dev_ws/src/build/cpp_pubsub/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/hiroaki-k4/my_project/ros2/dev_ws/src/cpp_pubsub
+# Build directory: /home/hiroaki-k4/my_project/ros2/dev_ws/src/build/cpp_pubsub
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
